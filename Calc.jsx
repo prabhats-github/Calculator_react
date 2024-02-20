910a54dbced5bfbc6580cc64fe08d7775c8c00aa
Prabhat Solanki <155136172+prabhats-github@users.noreply.github.com>
2024-02-20 15:13:04 +0530

    Add files via upload

---
+++ Calc.jsx
@@ -0,0 +1,62 @@
+import React, { useState } from 'react'
+import st from './Calc.module.css'
+
+const Calc = () => {
+
+  let [value,setValue]=useState("")
+
+  function handle(e){
+    setValue(value +e.target.innerHTML)
+  }
+
+  function eql(){
+    setValue(eval(value))
+  }
+  return (
+    <div className={st.maindiv}>
+        <div className={st.input}>
+            <input type="text" placeholder={value} />
+        </div >
+        <div id={st.one}>
+        <div className={st.four} onClick={handle}>+</div>
+        <div className={st.four} onClick={handle}>-</div>
+        <div className={st.four} onClick={handle}>*</div>
+        <div className={st.four} onClick={handle}>/</div>
+        </div>
+
+        <div id={st.equal} onClick={eql}>=</div>
+        
+        <div id={st.two}>
+        <div className={st.four} onClick={handle}>7</div>
+        <div className={st.four} onClick={handle}>8</div>
+        <div className={st.four} onClick={handle}>9</div>
+        
+        </div>
+
+        <div id={st.three}>
+        <div className={st.four} onClick={handle}>4</div>
+        <div className={st.four} onClick={handle}>5</div>
+        <div className={st.four} onClick={handle}>6</div>
+        </div>
+ 
+
+        <div id={st.four}>
+        <div className={st.four} onClick={handle}>1</div>
+        <div className={st.four} onClick={handle}>2</div>
+        <div className={st.four} onClick={handle}>3</div>
+        </div>
+
+
+        <div id={st.five}>
+        <div className={st.four}>0</div>
+        <div className={st.four}>.</div>
+        <div className={st.four}>AC</div>
+
+        </div>
+
+
+    </div>
+  )
+}
+
+export default Calc
